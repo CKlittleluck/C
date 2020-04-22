c45024ffc601e25ce17f3bd348d5ecf73897380e
CKlittleluck <1130018485@qq.com>
2020-04-22 13:03:33 +0800

    更新

---
+++ 4-22/4-22/源.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+#include<assert.h>
+
+//´íÎóÊ¹ÓÃmemcpyº¯Êý
+void * my_memcpy(void * dest, const void * src, size_t count)
+{
+	void * ret = dest;
+	assert(dest);
+	assert(src);
+	while (count--)
+	{
+		*(char *)dest = *(char *)src;
+		++(char *)dest;
+		++(char *)src;
+	}
+	return(ret);
+}
+int main()
+{
+	int i = 0;
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	my_memcpy(arr+2, arr, 20);
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
