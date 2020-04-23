cd306a4f1af545508f92c588edc07a7bacb6e76a
CKlittleluck <1130018485@qq.com>
2020-04-23 17:57:30 +0800

    结构体大小

---
+++ 4-23/4-23/源.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+#include <stdio.h>
+
+//struct S3
+//	{
+//		double a;
+//		char b;
+//		int c;
+//	};
+//struct S4
+//	{
+//		char c;
+//		struct S3 s3;
+//		double d;
+//	};
+//int main()
+//{
+//	struct S4 s4;
+//	printf("%d\n", sizeof(s4));
+//	return 0;
+//}
+
+
+//设置默认对齐数位4
+//#pragma pack(4)
+//struct S
+//{
+//	char a;
+//	int b;
+//	double c;
+//};
+//#pragma pack()
+//int main()
+//{
+//	struct S s;
+//	printf("%d\n", sizeof(s));//
+//	return 0;
+//}
