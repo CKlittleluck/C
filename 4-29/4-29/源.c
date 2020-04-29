64c7b9ce2e0a80de7fe288c772e906b7dd827aaa
CKlittleluck <1130018485@qq.com>
2020-04-29 22:00:17 +0800

    更新

---
+++ 4-29/4-29/源.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+/*int fib(int n)
+{
+if (n <= 2)
+return 1;
+else
+return fib(n - 1) + fib(n - 2);
+}
+int main()
+{
+int a, n;
+printf("输入数字: ");
+scanf("%d", &n);
+a = fib(n);
+printf("%d", a);
+return 0;
+}
+
+/*int fib(int n)
+{
+int result;
+int result1;
+int result2;
+result = result1 = 1;
+while (n > 2)
+{
+n -= 1;
+result2 = result1;
+result1 = result;
+result = result1 + result2;
+}
+return result;
+}
+int main()
+{
+int a , n;
+printf("输入数字: ");
+scanf("%d", &n);
+a = fib(n);
+printf("%d", a);
+return 0;
+}*/
