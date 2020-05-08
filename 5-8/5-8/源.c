787556bb281e036f32d4d267ad03bdc4f92bb774
CKlittleluck <1130018485@qq.com>
2020-05-08 22:39:49 +0800

    更新

---
+++ 5-8/5-8/源.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+//void print(int *p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//{
+//		printf("%d ", *(p + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	return 0;
+//}
+
+//×Ö·û´®ÄæÐò
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//void reverse_string(char* arr)
+//{
+//	char tmp = arr[0];
+//	int len = my_strlen(arr);
+//	arr[0] = arr[len - 1];
+//	arr[len - 1] = '\0';
+//    if (my_strlen(arr+1) >=2)
+//		reverse_string(arr+1);
+//	arr[len - 1] = tmp;
+//}
+//int main()
+//{
+//	char arr[] = "abcdefg";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
