14252754d1213772b6df969d6f5c6a3a7cb44f70
CKlittleluck <1130018485@qq.com>
2020-05-17 22:05:24 +0800

    更新

---
+++ 5-17/5-17/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+#define MAX(X, Y)     ((X)>(Y)?(X):(Y))
+int main()
+{
+	int a = 10;
+	int b = 11;
+
+	int max = MAX(a++, b++);
+	//int max = ((a++) > (b++) ? (a++) : (b++));
+
+	printf("%d\n", max);
+	printf("%d\n", a);
+	printf("%d\n", b);
+	return 0;
+}
