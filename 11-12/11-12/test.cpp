a27dae7f27a93a929440bc228ddd5199a792f9a3
CKlittleluck <1130018485@qq.com>
2020-11-13 15:03:06 +0800

    更新

---
+++ 11-12/11-12/test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <map>
+
+//电话号码重组
+class Soiution{
+public:
+	map<char, string> M = {
+		{ '2', "abc" }, { '3', "def" }, { '4', "ghi" }, { '5', "jkl"},
+		{ '6', "mno" }, { '7', "pqrs" }, { '8', "tuv" }, { '9', "wxyz" }
+	};
+	vector<string> ans;
+	string current;
+
+	void DFS(int index, string digits){
+		if (index == digits.size()){
+			ans.push_back(current);
+			return;
+		}
+
+		for (int i = 0; i < M[digits[index]].size(); i++){
+			current.push_back(M[digits[index]][i]);
+			DFS(index + 1, digits);
+			current.pop_back();
+		}
+	}
+
+	vector<string> letterCombinations(string digits){
+		if (digits.size() == 0){
+			return ans;
+		}
+		DFS(0, digits);
+		return ans;
+	}
+};
