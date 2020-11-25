231082c1a53e8e5b60b8b34366132dd7f005e938
CKlittleluck <1130018485@qq.com>
2020-11-25 16:19:49 +0800

    更新

---
+++ 11-23/11-23/test.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include<string>
+#include <algorithm>
+
+
+
+//·´×ª×Ö·û´®
+//kangkang yao jia you o
+//o you jia yao kangkang
+
+int main()
+{
+	string s;
+	getline(cin, s); 
+	reverse(s.begin(), s.end());
+	auto start = s.begin();
+	while (start != s.end())
+	{
+		auto end = start;
+		while (end != s.end() && *end != ' ')
+			end++;
+		reverse(start, end);
+		if (end != s.end())
+			start = end + 1;
+		else
+			start = end;
+	}
+	cout << s << endl;
+	return 0;
+}
+
+
