a010aa9ebc7bf08a8a22c5f896d062fd91c42b97
CKlittleluck <1130018485@qq.com>
2021-03-15 21:50:53 +0800

    对称二叉树

---
+++ 21.3-15/21.3-15/test.cpp
@@ -0,0 +1,15 @@
+
+
+//¶Ô³Æ¶þ²æÊ÷
+class Solution {
+public:
+	bool check(TreeNode *p, TreeNode *q) {
+		if (!p && !q) return true;
+		if (!p || !q) return false;
+		return p->val == q->val && check(p->left, q->right) && check(p->right, q->left);
+	}
+
+	bool isSymmetric(TreeNode* root) {
+		return check(root, root);
+	}
+};
