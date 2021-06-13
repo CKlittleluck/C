8f7ffae111c24b4661b1a3f5f75fb7d4fb6adfc4
CKlittleluck <1130018485@qq.com>
2021-06-13 20:40:28 +0800

    更新

---
+++ 21.6-13/21.6-13/test.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int minDistance(const string str1, const string str2)
+{
+	if (str1.empty() || str2.empty())
+		return max(str1.size(), str2.size());
+	int len1 = str1.size();
+	int len2 = str2.size();
+	vector<vector<int>> f(len1 + 1, vector<int>(len2 + 1, 0));
+
+	for (int i = 0; i <= len1; ++i)
+		f[i][0] = i;
+
+	for (int j = 0; j <= len2; ++j)
+		f[0][j] = j;
+
+	for (int i = 1; i <= len1; ++i)
+	{
+		for (int j = 1; j <= len2; ++j)
+		{
+			if (str2[j - 1] == str1[i - 1])
+			{
+				f[i][j] = 1 + min(f[i - 1][j], f[i][j - 1]);
+				f[i][j] = min(f[i][j], f[i - 1][j - 1]);
+			}
+			else
+			{
+				f[i][j] = 1 + min(f[i - 1][j], f[i][j - 1]);
+				f[i][j] = min(f[i][j], 1 + f[i - 1][j - 1]);
+			}
+		}
+	}
+	return f[len1][len2];
+}
+
+
+int main()
+{
+	string str1, str2;
+	while (cin >> str1 >> str2)
+	{
+		cout << minDistance(str1, str2) << endl;
+	}
+	return 0;
+}
