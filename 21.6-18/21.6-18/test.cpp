47a3b8139921ec6224f052692f7e51cdc07283ec
CKlittleluck <1130018485@qq.com>
2021-06-21 16:33:22 +0800

    更新

---
+++ 21.6-18/21.6-18/test.cpp
@@ -0,0 +1,22 @@
+//ºï×Ó·ÖÌÒ
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		if (n == 0)
+		{
+			break;
+		}
+
+		long total = pow(5, n) - 4;
+		long left = pow(4, n) + n - 4;
+		cout << total << " " << left << endl;
+	}
+	return 0;
+}
