fc2ffd7b9481d72481555e9a6b2a295a7cb3bf28
CKlittleluck <1130018485@qq.com>
2021-07-12 15:25:04 +0800

    更新

---
+++ 21.7-11.1/21.7-11.1/test.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		vector<string> path(n);
+		for (int i = 0; i < n; i++)
+		{
+			cin >> path[i];
+		}
+
+		sort(path.begin(), path.end());
+
+		vector<bool> flag(n, true);
+
+		for (int i = 0; i < n - 1; i++)
+		{
+			if (path[i] == path[i + 1])
+			{
+				flag[i] = false;
+			}
+			if (path[i] < path[i + 1]
+				&& path[i] == path[i + 1].substr(0, path[i].size())
+				&& path[i + 1][path[i].size()] == '/')
+			{
+				flag[i] = false;
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			if (flag[i])
+			{
+				cout << "mkdir -p " << path[i] << endl;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
