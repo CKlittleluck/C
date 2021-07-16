5c4cf5dfe3ad7381ba5eb4a0a1c2e39835efdd6c
CKlittleluck <1130018485@qq.com>
2021-07-16 13:03:24 +0800

    更新

---
+++ 21.7--10/21.7--10/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+#if 0
+int main()
+{
+	stringstream ss;
+	ss << "hello";
+	ss << "     ";
+	ss << "world!";
+
+	string s;
+
+	s = ss.str();
+	cout << s << endl;
+	return 0;
+}
+
+#endif
+
+
